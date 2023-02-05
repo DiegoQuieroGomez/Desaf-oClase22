2c40f764111185e8a784944f511b05fc89601580
Diego Quiero <106273507+DiegoQuieroGomez@users.noreply.github.com>
2023-02-05 14:43:53 -0300

    Productos con Faker

--- rutes/routeProducto.js
+++ rutes/routeProducto.js
@@ -1,6 +1,6 @@
 import express, {Router, json, urlencoded} from 'express'
-
-
+import { faker } from '@faker-js/faker';
+faker.locale ='es'
 
 const routerProductos = express.Router()
 routerProductos.use(express.json())
@@ -8,9 +8,19 @@ routerProductos.use(express.urlencoded({extended: true}))
 
 routerProductos.get('/', (req, res) => {
 
+    let objetos = []
+    for (let i = 0; i < 5; i++ ) {  
+        let nombre = faker.commerce.productName()
+        let precio = faker.commerce.price(1000,6000,0,'$')
+        let foto = faker.internet.domainName()
+        objetos.push({nombre, precio, foto})
+    }
+    console.log(objetos)
+    res.render('tabla.handlebars', {objetos})
+
 })
 
-
+//{nombre: 'diego', precio: 1000, foto:'www.loquyese.com'}
 
 
 export default routerProductos
